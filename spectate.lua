70ad8dfb3a80ca8d2503d299a8c56daaf046a56d
savageducks90-droid <savageducks90@gmail.com>
2026-03-11 23:16:24 -0700

    Update spectate.lua

--- spectate.lua
+++ spectate.lua
@@ -1,7 +1,9 @@
 local Players = game:GetService("Players")
+
 local player = Players.LocalPlayer
 local camera = workspace.CurrentCamera
 
+-- remove old gui
 if player.PlayerGui:FindFirstChild("SpectateMenu") then
 	player.PlayerGui.SpectateMenu:Destroy()
 end
@@ -19,10 +21,28 @@ frame.Parent = gui
 local layout = Instance.new("UIListLayout")
 layout.Parent = frame
 
+local currentSpectate = nil
+
+local function returnToSelf()
+
+	if player.Character and player.Character:FindFirstChild("Humanoid") then
+		camera.CameraSubject = player.Character.Humanoid
+	end
+
+	currentSpectate = nil
+
+end
+
 local function spectate(plr)
+
+	if currentSpectate == plr then
+		returnToSelf()
+		return
+	end
 
 	if plr.Character and plr.Character:FindFirstChild("Humanoid") then
 		camera.CameraSubject = plr.Character.Humanoid
+		currentSpectate = plr
 	end
 
 end
@@ -34,6 +54,8 @@ for _,plr in pairs(Players:GetPlayers()) do
 		local button = Instance.new("TextButton")
 		button.Size = UDim2.new(1,0,0,25)
 		button.Text = plr.Name
+		button.BackgroundColor3 = Color3.fromRGB(40,40,40)
+		button.TextColor3 = Color3.new(1,1,1)
 		button.Parent = frame
 
 		button.MouseButton1Click:Connect(function()
